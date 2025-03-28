f5f0d84c4b20573e7a56c7d97a948ac26c4f234b
sousa-p <p.soousa@gmail.com>
2025-03-28 16:49:37 -0300

    :rocket: feat: exceptions.py

---
+++ llms_selenium_automations/exceptions.py
@@ -0,0 +1,14 @@
+class EmptyChatException(Exception):
+  def __init__(self, message="Error handling chat history", field=None):
+    super().__init__(message)
+    self.field = field
+
+class ConnectionFailedException(Exception):
+  def __init__(self, message="Error trying to connect to LLM site", field=None):
+    super().__init__(message)
+    self.field = field
+
+class RequestFailedException(Exception):
+  def __init__(self, message="Error requesting prompt", field=None):
+    super().__init__(message)
+    self.field = field
